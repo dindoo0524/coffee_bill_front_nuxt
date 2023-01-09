de2c811b8edd494a6fba30690474a509cbb54231
나진주 <jinju@liveklass.com>
2023-01-09 22:36:54 +0900

    BaseUrl 변수 설정

--- lib/api/rest.ts
+++ lib/api/rest.ts
@@ -1,7 +1,6 @@
 // @ts-ignore
 import axios, { AxiosResponse, CancelTokenSource } from 'axios'
-const BaseUrl = process.env.NUXT_ENV_REST_AWS_URL
-console.log('BaseUrl process.env.NUXT_ENV_REST_AWS_URL', process.env.NUXT_ENV_REST_AWS_URL)
+const BaseUrl = 'http://52.78.230.246:3000'
 // const BaseUrl = process.env.NUXT_ENV_REST_URL  // 로컬
 
 type RestConfiguration = {
